a09e65f3adfa477b1fa0aa74a16cd94b1b68015a
Greg Fitzgerald <garious@gmail.com>
2016-11-18 23:54:23 -0700

    Move coverage

--- refbox/ui_test.py
+++ refbox/ui_test.py
@@ -1,4 +1,10 @@
 from . import ui
+from .gamemanager import GameManager
+from .noiomanager import IOManager
 
 def test_sized_frame():
     assert ui.sized_frame(None, 1, 2)
+
+def test_normal_view():
+    nv = ui.NormalView(GameManager(), IOManager(), NO_TITLE_BAR=True)
+    assert nv.first_game_started is False
