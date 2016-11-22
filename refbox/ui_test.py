d1e3e261c46298efb9a8db3dae499ced5d3e1c8a
Greg Fitzgerald <garious@gmail.com>
2016-11-21 21:58:49 -0700

    Format code with autopep8

--- refbox/ui_test.py
+++ refbox/ui_test.py
@@ -6,9 +6,11 @@ from .noiomanager import IOManager
 def test_sized_frame():
     assert ui.sized_frame(None, 1, 2)
 
+
 def test_score_column():
     root = ui.sized_frame(None, 1, 2)
     assert ui.ScoreColumn(root, 2, 'black', 'blue', 5, lambda: 42)
+
 
 def test_normal_view():
     nv = ui.NormalView(GameManager(), IOManager(), NO_TITLE_BAR=True)
